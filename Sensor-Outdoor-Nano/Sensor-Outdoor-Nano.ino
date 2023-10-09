7f0d34b46a73c413f262d03251715652606c4cc8
Elliot HH <eharhar06@proton.me>
2023-10-08 17:38:42 -0700

    tried a way but its not working
    also i realized after that i didnt put a second argument in the send function
    this shouldnt have had any effect tho as the serial output was wacko anyway - i never checked what the uno was seeing

--- Sensor-Outdoor-Nano/Sensor-Outdoor-Nano.ino
+++ Sensor-Outdoor-Nano/Sensor-Outdoor-Nano.ino
@@ -28,7 +28,8 @@ int count = 1;
 // Radio
 RCSwitch radio = RCSwitch();
 String data; // data to send 
-int currentVal; // the value of the current index to be transmitted - needed to transcode commas and periods
+int currentVal; // the value of the two numbers to be transmitted - helps with reconstructing on the receiver
+int tempPeriod; // store location of decimal in temp data
 
 void setup() {
   Serial.begin(9600);
@@ -54,14 +55,53 @@ void transmitData(){
   if (currentMillis - prevRadioTime > 10000 ) {
     prevRadioTime = currentMillis;
 
-    Serial.println(data);
-    
-    for (int i = 0; i < data.length(); i ++){
-      radio.send(int(data[i]), 24);
-      Serial.println(int(data[i]));
-      delay(200);
+    //sends data two digits at a time
+    Serial.println(tempC);
+    //Integer of temp
+    for (int i = 0; i < str_tempC.length(); i ++){ // loop through temp
+      if (str_tempC[i] == "."){ // find the period
+        tempPeriod = i; // store this value
+        for (int j = 0; j < i; j ++){ // loop through everything before the period
+          currentVal += str_tempC[j]; // add that to a string to be transmitted
+        }
+      }
     }
-    Serial.println("Sent ig");
+    radio.send(int(currentVal)); // send integer part of temp
+    Serial.println(currentVal);
+    Serial.println(int(currentVal));
+    currentVal = 0;
+
+    //Decimal of temp
+    for (int i = tempPeriod; i < str_tempC.length(); i ++){ // loop through temp starting at the period
+      currentVal += str_tempC[i]; // add to a string to be transmitted
+    }
+    radio.send(int(currentVal)); // send the decimal part of temp
+    Serial.println(currentVal);
+    Serial.println(int(currentVal));
+    currentVal = 0;
+
+    //Again but for humidity now - doesnt have decimal
+    Serial.println(humidity);
+
+    for (int i = 0; i < str_humidity.length(); i ++){ // loop through humidity
+      if (str_humidity[i] == "."){ // find the period
+        for (int j = 0; j < i; j ++){ // loop through everything before the period
+          currentVal += str_humidity[j]; // add that to a string to be transmitted
+        }
+      }
+    }
+    radio.send(int(currentVal)); // send humidity
+    Serial.println(currentVal);
+    Serial.println(int(currentVal));
+    currentVal = 0;
+    
+    //sends data one digit at a time
+    // for (int i = 0; i < data.length(); i ++){
+    //   radio.send(int(data[i]), 24);
+    //   Serial.println(int(data[i]));
+    //   delay(200);
+    // }
+    Serial.println("\nSent ig");
   }
 }
 void updateDHT() {  // read humidity and temperature from DHT sensor
